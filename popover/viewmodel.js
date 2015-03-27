ad34bc38a8e0e5ce200e0e701fa8f791ff014e8a
Juan Orozco <juanthedesigner@gmail.com>
2015-03-27 02:50:08 -0500

    Created initial component.

---
+++ popover/viewmodel.js
@@ -0,0 +1,57 @@
+import can from 'can';
+import 'can/map/define/define';
+
+export default can.Map.extend({
+    define: {
+        
+		/**
+		 * @property {boolean} popover.viewModel.visible visible
+         * @parent popover/viewModal
+		 * @description Controls visibility of modal
+		 * @option {boolean} Defaults to `false`.
+		 */
+         visible: {
+            value: false,
+            type: 'boolean'
+        },
+            
+		/**
+		 * @property {string} popover.viewModel.popoverId popoverId
+         * @parent modal/viewModal
+		 * @description popover id attribute value
+		 * @option {string} defaults to popover + <random number>
+		 */
+        popoverId: {
+            value: function () {
+                return 'popover' + Math.floor( Math.random() * 1000000 );
+            }
+        },
+            
+		/**
+		 * @property {string} popover.viewModel.placement placement
+         * @parent modal/viewModal
+		 * @description The placement of the popover. Must be bottom, top, left, right.
+		 * @option {string} Defaults to top string.
+		 */
+        placement: {
+            value: 'top',
+            type: 'string',
+            set: function (val) {
+                return (val === 'bottom' || val === 'top' || val === 'left' || val === 'right' ) ?
+                        val :
+                        'top';
+            }
+        }
+    },
+    
+    /**
+     * @function popover.viewModel.toggle toggle
+     * @parent popover/viewModel
+     * @description Toggle the popover
+     * @param {boolean} force Forces the visiblity of popover
+     */
+    toggle: function (force) {
+        force = force || !this.attr('visible');
+        this.attr('visible', force);
+    }
+});
